605d3b464fba33f2fa0ce6ea360c73e9c7224b66
c0a241459b <c0a242459b@edu.teu.ac.jp>
2025-11-18 16:59:11 +0900

    練習10:押下とん

--- pygame_image.py
+++ pygame_image.py
@@ -19,20 +19,37 @@ def main():
         
     bg_x = 0
     
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300, 200
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((1, 0))
+
+
+
         bg_x -= 1
         if bg_x <= -3200:
             bg_x = 0
         screen.blit(bg_img, (bg_x, 0))
         screen.blit(bg_img2, (bg_x + 1600, 0))
         screen.blit(bg_img, (bg_x + 3200, 0))
-        screen.blit(kt_img, [300, 200])
+        screen.blit(kt_img, kt_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(400)
+        clock.tick(200)
+        
         
         
 
